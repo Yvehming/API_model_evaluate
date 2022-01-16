ffa8967ad4934b2ab03e93966c7d2b622e62bab3
Yvehming <chang.t.z@qq.com>
2022-01-16 18:41:52 +0800

    use tf.compat.v1 to read trainable variable of saved model

--- summary.py
+++ summary.py
@@ -1,7 +1,15 @@
-import tensorflow as tf
-import tensorflow_hub as hub
+# https://www.cnblogs.com/CheeseZH/p/12620404.html
 
-model = tf.compat.v1.keras.models.load_model("pepper_output_inference_graph_v1.pb/saved_model", custom_objects={'KerasLayer':hub.KerasLayer}, compile=False)
-# model = tf.compat.v1.keras.models.load_model("pepper_output_inference_graph_v1.pb/saved_model")
-# model = tf.keras.models.load_model("pepper_output_inference_graph_v1.pb/saved_model")
-model.summary()
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+node = []
+saved_model_dir = "pepper_inference_graph_v2.pb/saved_model"
+with tf.Session(graph=tf.Graph()) as sess:
+    tf.saved_model.loader.load(sess, ["serve"], saved_model_dir)
+    graph = tf.get_default_graph()
+    for n in tf.get_default_graph().as_graph_def().node:
+        node.append(n.name)
+    print(node)
+    # t2 = tf.get_default_graph().get_tensor_by_name(name='FeatureExtractor/MobilenetV1/Conv2d_0/weights:0')
+    variable_name = [c.name for c in tf.trainable_variables()]
+    print(variable_name)
